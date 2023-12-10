28d018e95faf1719bc66849bb995eda5018d6975
Ethan <136099064+eemeidinger@users.noreply.github.com>
2023-12-09 21:32:41 -0500

    Update st_class.py

--- st_class.py
+++ st_class.py
@@ -43,8 +43,7 @@ class ImageProcessorApp:
         self.run()
 
     def image(self):
-        unique_key = "file_uploader_" + str(hash(self))  # Generate a unique key based on the object's hash
-        self.uploaded_file = st.file_uploader("Choose a png file", key = unique_key)
+        self.uploaded_file = st.file_uploader("Choose a png file")
         if self.uploaded_file is not None:
             # Read the file data into a BytesIO object
             bytes_io = io.BytesIO(self.uploaded_file.getvalue())
